bb91ac317042c9070de52fcba538d6c31d6a8c97
ashvinigjadhav <ashvinigjadhav@gmail.com>
2025-04-22 15:08:12 +0530

    Remove unnecessary context from landing page view

--- starbook/views.py
+++ starbook/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
 
 def landing_page(request):
-    context = {
-        'grid_items': range(1, 10),  # Pass a range of numbers to the template
-    }
-    return render(request, 'landing/index.html', context)
+ return render(request, 'landing/index.html')
